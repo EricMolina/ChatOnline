48d96aedb01e54effecb6537df5ef18a8919f554
Ricard Casals <richicasals@gmail.com>
2024-01-12 15:34:40 +0100

    added delete messahe proc

---
+++ proc/delete_message.php
@@ -0,0 +1,25 @@
+<?php
+
+include("../proc/utils.php");
+include("../proc/db_connection.php");
+
+session_start();
+
+check_user_login("./view/login.php");
+
+
+$message_id = $_POST['message_id'];
+
+try {
+    $query_delete_message = "DELETE FROM message
+                             WHERE id = :message_id";
+    
+    $stmt_delete_message = $conn->prepare($query_delete_message);
+    $stmt_delete_message->bindParam("message_id", $message_id);
+    $stmt_delete_message->execute();
+
+}  catch (PDOException $e) {
+    echo "Error al eliminar mensaje: ".$e->getMessage();
+    die();
+}
+
